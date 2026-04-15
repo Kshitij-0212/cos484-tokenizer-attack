d14ba9c0d2b713387ff552d61e6ba2dfcc34e396
Kshitij-0212 <64787002+Kshitij-0212@users.noreply.github.com>
2026-04-15 17:19:55 -0400

    Adding setup file

---
+++ setup.sh
@@ -0,0 +1,4 @@
+curl -LO https://github.com/alisawuffles/tokenizer-attack/releases/download/exp-subset/experiments_sample.tar.xz
+mkdir -p experiments
+tar -xf experiments_sample.tar.xz -C experiments
+rm experiments_sample.tar.xz
